9cea93736314e68332fa2a34d9b64f636de042f3
William Wayland <gutrixhd@gmail.com>
2023-01-27 18:01:10 +1000

    Returning result from main so it no longer panics.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::error::Error;
+
 mod solutions;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     for i in 1..=25 {
-        let (a, b) = solutions::s(i).unwrap();
+        let (a, b) = solutions::s(i)?;
         println!("{} - {:>5?}\t{:>5?}", i, a.unwrap_or(0), b.unwrap_or(0));    
-    } 
+    }
+    Ok(())
 }
