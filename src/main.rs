0b56695a2b4083deeb471d787e9bc5351ba22e18
William Wayland <gutrixhd@gmail.com>
2023-01-28 10:33:26 +1000

    Improved formatting of output.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod solutions;
 fn main() -> Result<(), Box<dyn Error>>{
     for i in 1..=25 {
         let (a, b) = solutions::s(i)?;
-        println!("{} - {:>5?}\t{:>5?}", i, a.unwrap_or(0), b.unwrap_or(0));    
+        println!("{} - {:>10?}\t{:>10?}", i, a.unwrap_or(0), b.unwrap_or(0));    
     }
     Ok(())
 }
